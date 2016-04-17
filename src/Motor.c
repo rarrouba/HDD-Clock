77649943ced26f0db0391dcdfe85635852a5d905
rarrouba <redouane.arroubai@gmail.com>
2016-04-17 17:08:03 +0200

    Update Motor.c

--- src/Motor.c
+++ src/Motor.c
@@ -8,32 +8,25 @@
 
 
 uint8_t count = 0;
-int graden =0;
-int ocrStap = 20;
-int beginOCRh = 1199;
-int beginOCRl = 38799;
-
+// 0°  => 0.388ms
+//90°  => 1.264ms
+//180°  => 2.140ms
 void Timer1_init()
 {
 	cli();//stop interrupts
-
-		//set timer1 interrupt at 1Hz
-
+	
 		  TCCR1A = 0;// set entire TCCR1A register to 0
 		  TCCR1B = 0;// same for TCCR1B
 		  TCNT1  = 0;//initialize counter value to 0
-		  // set compare match register for 416hz increments
-		  OCR1A = 37471;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+		  // set compare match register for (20 ms-1.264ms)*2 increments
+		  OCR1A = 37471;
 		  // turn on CTC mode
 		  TCCR1B |= (1 << WGM12);
-		  // Set CS10 and CS12 bits for 1 prescaler
+		  // Set CS10 and CS12 bits for 8 prescaler
 		  TCCR1B |= (1 << CS01) ;
 		  // enable timer compare interrupt
 		  TIMSK1 |= (1 << OCIE1A);
-
-
-
-
+		  
 		sei();//allow interrupts
 }
 
@@ -64,15 +57,15 @@ ISR(TIMER1_COMPA_vect)
 	    if (count % 2 == 0){
 
 	    	PORTB &= (0<<DDB1);
-
+		//(20 ms-1.264ms)*2 increments
 	    	OCR1A = 37471;
 
 		}
 
 		else{
-			  PORTB |= (1<<DDB1);
-
-				  OCR1A = 2527;
+		PORTB |= (1<<DDB1);
+		//(1.264ms)*2 increments
+		OCR1A = 2527;
 
 
 		}
