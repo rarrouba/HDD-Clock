ae2bb9601e5e9283dbd21c230c4b5984ac3c87bb
mwbaert <mattijs.baert@ugent.be>
2016-04-28 17:08:05 +0200

    Added files via upload

---
+++ LEDfunctie.h
@@ -0,0 +1,13 @@
+/*
+ * LEDfunctie.h
+ *
+ *  Created on: 26-apr.-2016
+ *      Author: Mattijs
+ */
+
+#ifndef SRC_LEDFUNCTIE_H_
+#define SRC_LEDFUNCTIE_H_
+
+void PrintCircle(char characters[16]);
+
+#endif /* SRC_LEDFUNCTIE_H_ */
