ef50c4f0d49ea22ddb1787f915f8cc0a520f2712
rarrouba <redouane.arroubai@gmail.com>
2016-05-08 20:31:50 +0200

    Added files via upload

--- src/main.c
+++ src/main.c
@@ -1,86 +1,12 @@
 
-#define LED_STRIP_PORT PORTB
-#define LED_STRIP_DDR  DDRB
-#define LED_STRIP_PIN  1
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include "HeaderFiles/Ledstrip.h"
+#include "HeaderFiles/dwenguinoBoard.h"
+#include "HeaderFiles/dwenguinoLCD.h"
 
-
-typedef struct grb_color
-{
-  unsigned char green,red, blue;
-} grb_color;
-
-
-void __attribute__((noinline)) led_strip_write(grb_color * colors, unsigned int count)
-{
-  LED_STRIP_PORT &= ~(1<<LED_STRIP_PIN);
-  LED_STRIP_DDR |= (1<<LED_STRIP_PIN);
-
-  cli();
-  while(count--)
-  {
-
-    asm volatile(
-
-        "ld __tmp_reg__, %a0+\n"
-        "rcall send_led_strip_byte%=\n"  // groen
-    	"ld __tmp_reg__, %a0+\n"
-
-        "rcall send_led_strip_byte%=\n"  // rood
-        "ld __tmp_reg__, %a0+\n"
-
-    	"rcall send_led_strip_byte%=\n"  // blauw
-        "rjmp led_strip_asm_end%=\n"
-
-        "send_led_strip_byte%=:\n"
-        "rcall send_led_strip_bit%=\n"  // (bit 7).
-        "rcall send_led_strip_bit%=\n"
-        "rcall send_led_strip_bit%=\n"
-        "rcall send_led_strip_bit%=\n"
-        "rcall send_led_strip_bit%=\n"
-        "rcall send_led_strip_bit%=\n"
-        "rcall send_led_strip_bit%=\n"
-        "rcall send_led_strip_bit%=\n"  // (bit 0).
-        "ret\n"
-
-
-        "send_led_strip_bit%=:\n"
-
-        "sbi %2, %3\n"                           // Drive the line high.
-
-
-        "rol __tmp_reg__\n"                      // Rotate left through carry.
-
-
-
-        "nop\n" "nop\n"
-
-
-        "brcs .+2\n" "cbi %2, %3\n"              // If the bit to send is 0, drive the line low now.
-
-
-
-        "nop\n" 								//"nop\n" "nop\n" "nop\n" "nop\n"
-
-
-        "brcc .+2\n" "cbi %2, %3\n"              // If the bit to send is 1, drive the line low now.
-
-        "ret\n"
-        "led_strip_asm_end%=: "
-        : "=b" (colors)
-        : "0" (colors),
-          "I" (_SFR_IO_ADDR(LED_STRIP_PORT)),
-          "I" (LED_STRIP_PIN)
-    );
-
-
-  }
-  sei();
-  _delay_us(15);
-}
 
 #define LED_COUNT 16
 grb_color colors[LED_COUNT];
@@ -88,11 +14,26 @@ grb_color colors[LED_COUNT];
 
 int main()
 {
+	initBoard();
+	initLCD();
+	backlightOn();
+	clearLCD();
+	Timer1_Init();
+	for (int i = 0; i < 140; i++)
+    {
+		OCR3A ++ ;
+		_delay_ms(20);
+
+	}
+	Timer3_Init();
+
+
   unsigned int count = 0;
   grb_color colorsSet[] = {(grb_color){  0,0, 0 },(grb_color){  127,255, 0 },(grb_color){  255,255, 0 },(grb_color){  255,127, 0 }, (grb_color){  255,0, 0 },(grb_color){  255,0, 127 },(grb_color){  255,0, 255 },(grb_color){  127,0, 255 },(grb_color){ 0, 0, 255 },(grb_color){ 0, 127, 255 },(grb_color){ 0, 255, 255 },(grb_color){ 0, 255, 127 },(grb_color){  0,255, 0 },(grb_color){  127,255, 0 },(grb_color){  255,255, 0 },(grb_color){  255,127, 0 }, (grb_color){  255,0, 0 },(grb_color){  255,0, 127 },(grb_color){  255,0, 255 }};
 
   while(1)
   {
+	  LCDSnelheid();
 	  unsigned int i;
 	  for(i = 0; i < LED_COUNT; i++)
 	  {
@@ -103,7 +44,9 @@ int main()
 	  led_strip_write(colors, LED_COUNT);
 	  count++;
 	  count = (count)%LED_COUNT;
-      _delay_ms(50);
+      _delay_ms(500);
 
   }
+
+
 }
