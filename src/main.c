bca8c06322710b17060f76a27a6950fc81c60d9f
mwbaert <mattijs.baert@ugent.be>
2016-05-12 16:44:33 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -19,10 +19,9 @@ int main()
 	initLCD();
 	backlightOn();
 	clearLCD();
-	Timer1_Init();
+	
 	
 	//motor
-	INT_init();
 	Timer1_Motor_Init();
 	Opstart_ESC();
 	for (int i = 0; i < 140; i++)
@@ -30,32 +29,15 @@ int main()
 		OCR1A ++ ;
 		_delay_ms(20);
 	}
-	for (int i = 0; i < 140; i++)
-    {
-		OCR3A ++ ;
-		_delay_ms(20);
-
-	}
 	Timer3_Init();
 
 
   unsigned int count = 0;
-  grb_color colorsSet[] = {(grb_color){  0,0, 0 },(grb_color){  127,255, 0 },(grb_color){  255,255, 0 },(grb_color){  255,127, 0 }, (grb_color){  255,0, 0 },(grb_color){  255,0, 127 },(grb_color){  255,0, 255 },(grb_color){  127,0, 255 },(grb_color){ 0, 0, 255 },(grb_color){ 0, 127, 255 },(grb_color){ 0, 255, 255 },(grb_color){ 0, 255, 127 },(grb_color){  0,255, 0 },(grb_color){  127,255, 0 },(grb_color){  255,255, 0 },(grb_color){  255,127, 0 }, (grb_color){  255,0, 0 },(grb_color){  255,0, 127 },(grb_color){  255,0, 255 }};
 
   while(1)
   {
 	  LCDSnelheid();
-	  unsigned int i;
-	  for(i = 0; i < LED_COUNT; i++)
-	  {
 
-		  colors[i] = colorsSet[(i+count)%LED_COUNT];
-	  }
-
-	  led_strip_write(colors, LED_COUNT);
-	  count++;
-	  count = (count)%LED_COUNT;
-      _delay_ms(500);
 
   }
 
